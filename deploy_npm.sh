ef0a636dfeb4ac2ae00c4017be66edd2052c6826
papu bhattacharya <papu.bhattacharya@synectiks.com>
2018-12-11 14:28:32 +0530

    Adding travis ci  integration file , and npm deployment script
    Adding travis ci  integration file , and npm deployment script

--- deploy_npm.sh
+++ deploy_npm.sh
@@ -1,16 +1,16 @@
 cd packages
-cd survey-angular
+cd xform-angular
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
-cd ../survey-jquery
+cd ../xform-jquery
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
-cd ../survey-knockout
+cd ../xform-knockout
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
-cd ../survey-react
+cd ../xform-react
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
-cd ../survey-vue
+cd ../xform-vue
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
